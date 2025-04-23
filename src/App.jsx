96960e4ef8b458af4b0d728c42dd40587739ba1a
alemawit <alemawit.woldu@gmail.com>
2025-04-23 10:24:20 -0400

    import files editted

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import { Routes, Route, Router } from "react-router-dom";
 //import the components
 import Home from "./markup/pages/Home";
 import Login from "./markup/pages/Login";
-import AddEmployee from "./markup/pages/admin/AddEmployee";
+import AddEmployee from "./markup/pages/Admin/AddEmployee";
 import Header from "./markup/components/Header/Header";
 import "./App.css";
 import Footer from "./markup/components/Footer/Footer";
-import Service from "./markup/pages/admin/Service";
+import Service from "./markup/pages/Admin/Service";
 import About from "./markup/pages/About";
 import Unautorized from "./markup/pages/Unautorized";
 //import the private auth route component
 import PrivateAuthRoute from "./markup/components/Auth/PrivateAuthRoute";
-import Orders from "./markup/pages/admin/Orders";
-import AddCustomers from "./markup/pages/admin/AddCustomers";
-import Employees from "./markup/pages/admin/Employees";
+import Orders from "./markup/pages/Admin/Orders";
+import AddCustomers from "./markup/pages/Admin/AddCustomers";
+import Employees from "./markup/pages/Admin/Employees";
 //import AdminDashboard from "./markup/pages/Admin/AdminDashboard";
-import Dashboard from "./markup/pages/admin/Dashboard";
+import Dashboard from "./markup/pages/Admin/Dashboard";
 import Contact from "./markup/pages/Contact";
-import Customers from "./markup/pages/admin/Customers";
-import EditEmployee from "./markup/pages/admin/EditEmployee";
-import EditCustomer from "./markup/pages/admin/EditCustomer";
-import AddNewOrder from "./markup/pages/admin/AddNewOrder";
+import Customers from "./markup/pages/Admin/Customers";
+import EditEmployee from "./markup/pages/Admin/EditEmployee";
+import EditCustomer from "./markup/pages/Admin/EditCustomer";
+import AddNewOrder from "./markup/pages/Admin/AddNewOrder";
 import Services from "./markup/pages/Services";
-import EditOrder from "./markup/pages/admin/EditOrder";
-import CustomerDetail from "./markup/pages/admin/CustomerDetail";
+import EditOrder from "./markup/pages/Admin/EditOrder";
+import CustomerDetail from "./markup/pages/Admin/CustomerDetail";
 
 function App() {
   return (
